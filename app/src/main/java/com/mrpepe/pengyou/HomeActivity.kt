a01b1633d3a44c9db59e35246377c21ddebcd8ee
Felipe Peter <mr-peipei@web.de>
2019-12-14 19:06:36 +0800

    Initial commit

---
+++ app/src/main/java/com/mrpepe/pengyou/HomeActivity.kt
@@ -0,0 +1,19 @@
+package com.mrpepe.pengyou
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+
+    fun openDict(view: View){
+        val intent = Intent(this, DictionaryActivity::class.java)
+        startActivity(intent)
+    }
+}
