207bdb77fab5a6c166acb08fbf5f70d7cd8a7f1d
Felipe Peter <mr-peipei@web.de>
2019-12-18 21:16:05 +0800

    Add tabbed activity to show word details

---
+++ app/src/main/java/com/mrpepe/pengyou/dictionary/WordViewActivity.kt
@@ -0,0 +1,30 @@
+package com.mrpepe.pengyou.dictionary
+
+import android.os.Bundle
+import com.google.android.material.tabs.TabLayout
+import androidx.viewpager.widget.ViewPager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProviders
+import com.mrpepe.pengyou.R
+import com.mrpepe.pengyou.dictionary.ui.wordViewUI.WordViewPagerAdapter
+import com.mrpepe.pengyou.dictionary.ui.wordViewUI.WordViewViewModel
+import kotlinx.android.synthetic.main.activity_word_view.*
+
+class WordViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_word_view)
+        val sectionsPagerAdapter = WordViewPagerAdapter(this, supportFragmentManager)
+        val viewPager: ViewPager = findViewById(R.id.view_pager)
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = findViewById(R.id.tabs)
+        tabs.setupWithViewPager(viewPager)
+
+        var model: WordViewViewModel = ViewModelProviders.of(this).get(WordViewViewModel::class.java)
+
+        model.entry = intent.extras?.get("entry") as Entry
+
+        headword.text = model.entry.simplified
+    }
+}
---
+++ app/src/main/java/com/mrpepe/pengyou/dictionary/ui/wordViewUI/WordDefinitionFragment.kt
@@ -0,0 +1,47 @@
+package com.mrpepe.pengyou.dictionary.ui.wordViewUI
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+
+import com.mrpepe.pengyou.R
+import com.mrpepe.pengyou.extractDefinitions
+import kotlinx.android.synthetic.main.activity_word_view.*
+import kotlinx.android.synthetic.main.fragment_word_definition.*
+import java.lang.Exception
+
+class WordDefinitionFragment : Fragment() {
+
+    private lateinit var model: WordViewViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        model = activity?.run {
+            ViewModelProviders.of(this)[WordViewViewModel::class.java]
+        } ?: throw Exception("Invalid Activity")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_word_definition, container, false)
+
+        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        definitions.text = extractDefinitions(model.entry.definitions)
+    }
+
+
+}
