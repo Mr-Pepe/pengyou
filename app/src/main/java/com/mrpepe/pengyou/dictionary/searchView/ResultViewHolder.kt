be04642a171a3c3bb7c9a3e283788eca861cce33
Felipe Peter <mr-peipei@web.de>
2020-01-13 18:45:54 +0800

    Show HSK information in search results

--- app/src/main/java/com/mrpepe/pengyou/dictionary/searchView/ResultViewHolder.kt
+++ app/src/main/java/com/mrpepe/pengyou/dictionary/searchView/ResultViewHolder.kt
@@ -11,11 +11,17 @@ class ResultViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private var headword: TextView = itemView.findViewById(R.id.headword)
     private var pinyin: TextView = itemView.findViewById(R.id.pinyin)
     private var definitions: TextView = itemView.findViewById(R.id.definitions)
+    private var hsk: TextView = itemView.findViewById(R.id.hsk)
 
     fun bind(entry: Entry, clickListener: (Entry) -> Unit) {
         headword.text = entry.simplified
         pinyin.text = entry.pinyin
         definitions.text = extractDefinitions(entry.definitions, false)
+
+        hsk.text = when (entry.hsk) {
+            7 -> ""
+            else -> "HSK " + entry.hsk.toString()
+        }
 
         itemView.setOnClickListener { clickListener(entry) }
     }
