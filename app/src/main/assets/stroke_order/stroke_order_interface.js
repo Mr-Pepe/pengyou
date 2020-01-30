77b5219d76e8a7aabe4b1427757136120ce37508
Felipe Peter <mr-peipei@web.de>
2020-01-30 19:14:07 +0800

    Put stroke order stuff in extra folder

--- app/src/main/assets/stroke_order/stroke_order_interface.js
+++ app/src/main/assets/stroke_order/stroke_order_interface.js
@@ -1,11 +1,18 @@
 function drawCharacter() {
-    var writer = HanziWriter.create('character-target-div', '上', {
+
+    var writer = HanziWriter.create('draw_board', '轮', {
       charDataLoader: function() {
         data = JSON.parse(Android.getJSON())
         console.log(data)
         return data
-      }
+      },
+      width: 200,
+      height: 200,
+      padding: 5,
+      delayBetweenLoops: 3000
     });
 
     writer.loopCharacterAnimation();
+
 }
+
