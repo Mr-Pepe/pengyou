43111974455ae519ef78b89a16ed733708236d06
Felipe Peter <mr-peipei@web.de>
2020-02-17 23:00:44 +0800

    Use bottom navigation view

--- app/src/main/java/com/mrpepe/pengyou/HomeActivity.kt
+++ app/src/main/java/com/mrpepe/pengyou/HomeActivity.kt
@@ -1,22 +1,77 @@
 package com.mrpepe.pengyou
 
 import android.content.Intent
+import android.content.res.Resources
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
+import android.widget.Toolbar
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.mrpepe.pengyou.dictionary.DictionaryFragment
 import com.mrpepe.pengyou.dictionary.searchView.SearchViewActivity
+import com.mrpepe.pengyou.settings.SettingsFragment
+import kotlinx.android.synthetic.main.activity_home.*
+import java.net.URI
 
-class HomeActivity : BaseActivity() {
+class HomeActivity : BaseActivity(),
+    DictionaryFragment.OnFragmentInteractionListener,
+    SettingsFragment.OnFragmentInteractionListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        val intent = Intent(this, SearchViewActivity::class.java)
-        startActivity(intent)
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.home_container_nav_host_fragment) as NavHostFragment
+
+        val navController = host.navController
+
+        AppBarConfiguration(navController.graph)
+
+        setupBottomNavMenu(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            val dest: String = try {
+                resources.getResourceName(destination.id)
+            } catch (e: Resources.NotFoundException) {
+                Integer.toString(destination.id)
+            }
+
+            Toast.makeText(this@HomeActivity, "Navigated to $dest", Toast.LENGTH_SHORT).show()
+            Log.d("HomeActivity", "Navigated to $dest")
+        }
+
     }
 
-    fun openDict(view: View){
-        val intent = Intent(this, SearchViewActivity::class.java)
-        startActivity(intent)
+    private fun setupBottomNavMenu(navController: NavController) {
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+        bottomNav?.setupWithNavController(navController)
+
+        bottomNav?.setOnNavigationItemSelectedListener { item ->
+            when (item.title) {
+                getString(R.string.bottom_navigation_view_dictionary) -> {
+                    findNavController(R.id.home_container_nav_host_fragment).navigate(R.id.dictionary)
+                }
+                getString(R.string.bottom_navigation_view_settings) -> {
+                    findNavController(R.id.home_container_nav_host_fragment).navigate(R.id.settings)
+                }
+                else -> {}
+            }
+
+            true
+        }
+    }
+
+    override fun onFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
